2cc14426925591ec0124b6a3f7d15836d49ff648
FlippingBinary <35066367+FlippingBinary@users.noreply.github.com>
2022-07-27 11:19:42 -0400

    Add content

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+  <link href="https://fonts.googleapis.com/css?family=Shadows+Into+Light" rel="stylesheet">
+  <title>WV Sports Legends Banquet 2019</title>
+</svelte:head>
+
+<div class="grid h-screen place-items-center">
+  <div class="text-center p-6 w-96">
+    <a href="/logo-color-2200.png"><img id="logo-480" src="/logo-color-480.png" alt="CSMR Club" title="CSMR Club"></a>
+    <h1 class="text-3xl font-bold block">A club at <a class="hover:underline" href="http://wvstateu.edu">WVSU</a> representing Computer Science, Mathematics, and
+      Robotics enthusiasts.</h1>
+  </div>
+</div>
+
+<style>
+  div {
+    font-family: 'Shadows Into Light', cursive;
+  }
+</style>
